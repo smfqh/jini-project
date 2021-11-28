9e2b730f521dac251f30a914a68ac2224e75b864
smfqh <93059809+smfqh@users.noreply.github.com>
2021-11-28 13:03:26 +0900

    Update jiniAutoCoinSell.py

--- jiniAutoCoinSell.py
+++ jiniAutoCoinSell.py
@@ -7,7 +7,6 @@ import jwt
 from urllib.parse import urlencode
 from decimal import Decimal
 import uuid
-import os
 import sys
 import math
 import pandas as pd
@@ -15,56 +14,121 @@ import numpy
 import hashlib
 
 # Keys
-
+access_key = "obxBT66Cx8fJsnww9TAfJwMKUx443RBiElaZRq1b"
+secret_key = "wKUSQ8GaxDDC1BNcPWrNBjYQIP7ncEyv07j4TXTV"
 server_url = 'https://api.upbit.com'
+
 
 min_order_amt = 5000
 
 sell_pcnt = 5
 
+dcnt_pcnt = -2
+
+# sell_except_item = ["LOOM"]  
 sell_except_item = []  
 
 
 def start_seconddream():
+
     try:
 
-        set_loglevel("E")    
-
+        set_loglevel("I")    
+ 
+        # ----------------------------------------------------------------------
+        # 반복 수행
+        # ----------------------------------------------------------------------
         while True:
-
-            # 1. my coin list
-            my_items = get_accounts('Y','KRW')
-            my_items_comma = chg_account_to_comma(my_items)
-            tickers = get_ticker(my_items_comma)
-
+ 
+            # ------------------------------------------------------------------
+            # 보유 종목조회
+            # ------------------------------------------------------------------
+            target_items = get_accounts('Y', 'KRW')
+ 
+            # ------------------------------------------------------------------
+            # 보유 종목 현재가 조회
+            # ------------------------------------------------------------------
+            target_items_comma = chg_account_to_comma(target_items)
+            tickers = get_ticker(target_items_comma)
+ 
             # -----------------------------------------------------------------
-            # my item
+            # 보유 종목별 진행
             # -----------------------------------------------------------------
-            for my_item in my_items:
+            for target_item in target_items:
                 for ticker in tickers:
-                    if my_item['market'].split('-')[1] not in sell_except_item :                         
-                        if my_item['market'] == ticker['market']:
-    
-                            rev_pcnt = round(((Decimal(str(ticker['trade_price'])) - Decimal(str(my_item['avg_buy_price']))) / Decimal(str(my_item['avg_buy_price']))) * 100, 2)
-    
-                            logging.info('')
+                    if target_item['market'] == ticker['market']:
+ 
+                        # -----------------------------------------------------
+                        # 수익률 계산
+                        # ((현재가 - 평균매수가) / 평균매수가) * 100
+                        # -----------------------------------------------------
+                        rev_pcnt = round(((Decimal(str(ticker['trade_price'])) - Decimal(str(target_item['avg_buy_price']))) / Decimal(str(target_item['avg_buy_price']))) * 100, 2)
+ 
+                        logging.info('')
+                        logging.info('------------------------------------------------------')
+                        logging.info('- 종목:' + str(target_item['market']))
+                        logging.info('- 평균매수가:' + str(target_item['avg_buy_price']))
+                        logging.info('- 현재가:' + str(ticker['trade_price']))
+                        logging.info('- 수익률:' + str(rev_pcnt))
+ 
+                        # -----------------------------------------------------
+                        # 현재 수익률이 매도 수익률 이상인 경우에만 진행
+                        # -----------------------------------------------------
+                        if Decimal(str(rev_pcnt)) < Decimal(str(sell_pcnt)):
+                            logging.info('- 현재 수익률이 매도 수익률 보다 낮아 진행하지 않음!!!')
                             logging.info('------------------------------------------------------')
-                            logging.info('- item:' + str(my_item['market']))
-                            logging.info('- avg_buy_price:' + str(my_item['avg_buy_price']))
-                            logging.info('- trade_price:' + str(ticker['trade_price']))
-                            logging.info('- rev_pcnt:' + str(rev_pcnt))
-
-                            if Decimal(str(rev_pcnt)) < Decimal(str(sell_pcnt)):
-                                logging.info('- under rev_pcnt!!!')
-                                logging.info('------------------------------------------------------')
-                                continue
-                            else :
-                                logging.info('sell start! [' + str(my_item['market']) + ']')
-                                rtn_sellcoin_mp = sellcoin_mp(my_item['market'], 'Y')
-                                logging.info('sell end ! [' + str(my_item['market']) + ']')
-                                logging.info(rtn_sellcoin_mp)
-                                logging.info('------------------------------------------------------')
-
+                            continue
+ 
+                        # -------------------------------------------------
+                        # 고점을 계산하기 위해 최근 매수일자 조회
+                        # 1. 해당 종목에 대한 거래 조회(done, cancel)
+                        # 2. 거래일시를 최근순으로 정렬
+                        # 3. 매수 거래만 필터링
+                        # 4. 가장 최근 거래일자부터 현재까지 고점을 조회
+                        # -------------------------------------------------
+                        order_done = get_order_status(target_item['market'], 'done') + get_order_status(target_item['market'], 'cancel')
+                        order_done_sorted = orderby_dict(order_done, 'created_at', True)
+                        order_done_filtered = filter_dict(order_done_sorted, 'side', 'bid')
+ 
+                        # ------------------------------------------------------------------
+                        # 캔들 조회
+                        # ------------------------------------------------------------------
+                        candles = get_candle(target_item['market'], 'D', 200)
+ 
+                        # ------------------------------------------------------------------
+                        # 최근 매수일자 다음날부터 현재까지의 최고가를 계산
+                        # ------------------------------------------------------------------
+                        df = pd.DataFrame(candles)
+                        mask = df['candle_date_time_kst'] > order_done_filtered[0]['created_at']
+                        filtered_df = df.loc[mask]
+ 
+                        higest_high_price = numpy.max(filtered_df['high_price'])
+ 
+                        # -----------------------------------------------------
+                        # 고점대비 하락률
+                        # ((현재가 - 최고가) / 최고가) * 100
+                        # -----------------------------------------------------
+                        cur_dcnt_pcnt = round(((Decimal(str(ticker['trade_price'])) - Decimal(str(higest_high_price))) / Decimal(str(higest_high_price))) * 100, 2)
+ 
+                        logging.info('- 매수 후 최고가:' + str(higest_high_price))
+                        logging.info('- 고점대비 하락률:' + str(cur_dcnt_pcnt))
+                        
+                        if Decimal(str(cur_dcnt_pcnt)) < Decimal(str(dcnt_pcnt)):
+                                
+                            # ------------------------------------------------------------------
+                            # 시장가 매도
+                            # 실제 매도 로직은 안전을 위해 주석처리 하였습니다.
+                            # 실제 매매를 원하시면 테스트를 충분히 거친 후 주석을 해제하시면 됩니다.
+                            # ------------------------------------------------------------------
+                            logging.info('시장가 매도 시작! [' + str(target_item['market']) + ']')
+                            rtn_sellcoin_mp = sellcoin_mp(target_item['market'], 'Y')
+                            logging.info('시장가 매도 종료! [' + str(target_item['market']) + ']')
+                            logging.info(rtn_sellcoin_mp)
+                            logging.info('------------------------------------------------------')
+ 
+                        else:
+                            logging.info('- 고점 대비 하락률 조건에 맞지 않아 매도하지 않음!!!')
+                            logging.info('------------------------------------------------------')
 
     except Exception:
         raise
